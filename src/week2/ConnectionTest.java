32eb8202ccca03251ec3cadae99148cc0706a2e3
stp008 <clack008@gmail.com>
2014-08-05 19:48:51 +0400

    refactoring

--- src/week2/ConnectionTest.java
+++ src/week2/ConnectionTest.java
@@ -20,30 +20,28 @@ public class ConnectionTest {
 
 		try {
 		    conn =   DriverManager.getConnection("jdbc:mysql://localhost/twitter?" +
-		                                   "user=root&password=12121212");
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-				
-		try {
+		                                   "user=root&password=12121212");		    
 		    stmt = conn.createStatement();
 		    rs = stmt.executeQuery("SELECT * FROM users");
 		    while(rs.next()) {
 		    	System.out.println(rs.getString(1) + " :: " + rs.getString(2));
 		    }
-
-		}
-		catch (SQLException ex){
+		} catch (SQLException ex) {
 		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+		    System.err.println("SQLException: " + ex.getMessage());
+		    System.err.println("SQLState: " + ex.getSQLState());
+		    System.err.println("VendorError: " + ex.getErrorCode());
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException ex) {
+				System.err.println("Unable to close the connection.");
+				System.err.println("SQLException: " + ex.getMessage());
+			    System.err.println("SQLState: " + ex.getSQLState());
+			    System.err.println("VendorError: " + ex.getErrorCode());
+			}
 		}
-		
-		
+								
 	}
 
 }
