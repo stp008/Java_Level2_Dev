b5d82858e8dfe8e27ba746132bc3a07ba5916151
stp008 <clack008@gmail.com>
2014-08-11 01:35:51 +0400

    removed unused imports

--- src/week3/financial_manager/view/authorization.java
+++ src/week3/financial_manager/view/authorization.java
@@ -4,30 +4,16 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-import java.awt.GridBagLayout;
-
 import javax.swing.JLabel;
 
-import java.awt.GridBagConstraints;
 
 import javax.swing.JTextField;
 
-import java.awt.Insets;
-
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
-
 import java.awt.GridLayout;
-import java.awt.BorderLayout;
+
 
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
-import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -35,14 +21,10 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-import org.eclipse.wb.swing.FocusTraversalOnArray;
-
-import java.awt.Component;
-import java.awt.Window.Type;
 import java.awt.Color;
 import java.awt.Toolkit;
 
-public class authorization {
+public class Authorization {
 
 	private JFrame frmFinancialManager;
 	private JTextField textField;
@@ -50,18 +32,22 @@ public class authorization {
 
 	/**
 	 * Launch the application.
-	 * @throws UnsupportedLookAndFeelException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws ClassNotFoundException 
+	 * 
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		//UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+		// UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		UIManager
+				.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					authorization window = new authorization();
+					Authorization window = new Authorization();
 					window.frmFinancialManager.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -73,7 +59,7 @@ public class authorization {
 	/**
 	 * Create the application.
 	 */
-	public authorization() {
+	public Authorization() {
 		initialize();
 	}
 
@@ -82,43 +68,49 @@ public class authorization {
 	 */
 	private void initialize() {
 		frmFinancialManager = new JFrame();
-		frmFinancialManager.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Just a man\\Desktop\\1399996127_Customer_Male_Light.png"));
+		frmFinancialManager
+				.setIconImage(Toolkit
+						.getDefaultToolkit()
+						.getImage(
+								"C:\\Users\\Just a man\\Desktop\\1399996127_Customer_Male_Light.png"));
 		frmFinancialManager.setResizable(false);
-		frmFinancialManager.setTitle("\u0424\u0438\u043D\u0430\u043D\u0441\u043E\u0432\u044B\u0439 \u043C\u0435\u043D\u0435\u0434\u0436\u0435\u0440");
+		frmFinancialManager
+				.setTitle("\u0424\u0438\u043D\u0430\u043D\u0441\u043E\u0432\u044B\u0439 \u043C\u0435\u043D\u0435\u0434\u0436\u0435\u0440");
 		frmFinancialManager.setBounds(100, 100, 277, 340);
 		frmFinancialManager.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmFinancialManager.getContentPane().setLayout(new GridLayout(8, 1, 3, 3));
-		
+		frmFinancialManager.getContentPane().setLayout(
+				new GridLayout(8, 1, 3, 3));
+
 		JLabel lblNewLabel_1 = new JLabel("\u041B\u043E\u0433\u0438\u043D");
 		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		frmFinancialManager.getContentPane().add(lblNewLabel_1);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		frmFinancialManager.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel label = new JLabel("");
 		label.setForeground(Color.RED);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		frmFinancialManager.getContentPane().add(label);
-		
+
 		JLabel lblNewLabel = new JLabel("\u041F\u0430\u0440\u043E\u043B\u044C");
 		lblNewLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		frmFinancialManager.getContentPane().add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		frmFinancialManager.getContentPane().add(textField);
 		textField.setColumns(1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setForeground(Color.RED);
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		frmFinancialManager.getContentPane().add(lblNewLabel_2);
-		
+
 		JButton btnNewButton = new JButton("\u0412\u0445\u043E\u0434");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -128,8 +120,9 @@ public class authorization {
 		});
 		btnNewButton.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		frmFinancialManager.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("\u0420\u0435\u0433\u0438\u0441\u0442\u0440\u0430\u0446\u0438\u044F");
+
+		JButton btnNewButton_1 = new JButton(
+				"\u0420\u0435\u0433\u0438\u0441\u0442\u0440\u0430\u0446\u0438\u044F");
 		btnNewButton_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		frmFinancialManager.getContentPane().add(btnNewButton_1);
 	}
